9b410c183d99444c97ece48899a0aabdace43fb3
Bolivar Delgado <bolivar.delgado@gmail.com>
2025-02-13 13:50:25 -0500

    feat: validation in user entity

--- src/main/java/org/eclipse/jakarta/entity/UserEntity.java
+++ src/main/java/org/eclipse/jakarta/entity/UserEntity.java
@@ -1,16 +1,34 @@
 package org.eclipse.jakarta.entity;
 
+import jakarta.persistence.Column;
+
 //import java.util.ArrayList;
 //import java.util.Collection;
 
 import jakarta.persistence.Entity;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 //import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 @Entity
 public class UserEntity extends AbstractEntity {
 
+  @Column(length = 100)
+  @NotEmpty(message="An email must be set")
+  @Email(message="Email must be in the format user@somedomain.com")
   private String email;
+
+  @NotBlank(message="Password cannot be empty")
+  @Size(min=8, max=100, message="Password must be a min of 8 and max of 100 characters")
+  @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[\\$%&#!]).{8,100}$", message="Password must have at least one upper case, " + "one lower case, and must contain at least one of $%&#!")
   private String password;
+
+  @NotEmpty(message="Name must be set")
+  @Size(min=2, max=100, message="Name must be a min of 2 and max 100 characters ")
   private String fullName;
 
   //@OneToMany
