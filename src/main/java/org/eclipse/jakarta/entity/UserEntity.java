c40fe99460bb27e706138840ed01f87e672945f4
Bolivar Delgado <bolivar.delgado@gmail.com>
2025-02-13 13:50:43 -0500

    feat: validation in user entity

--- src/main/java/org/eclipse/jakarta/entity/UserEntity.java
+++ src/main/java/org/eclipse/jakarta/entity/UserEntity.java
@@ -10,7 +10,6 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 //import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
