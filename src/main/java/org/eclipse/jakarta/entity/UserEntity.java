7571759bb5f3543beb808ad8bd2f8b5a63e0f86c
Bolivar Delgado <bolivar.delgado@gmail.com>
2025-02-13 12:39:42 -0500

    feat: get and set in user entity

--- src/main/java/org/eclipse/jakarta/entity/UserEntity.java
+++ src/main/java/org/eclipse/jakarta/entity/UserEntity.java
@@ -1,5 +1,43 @@
 package org.eclipse.jakarta.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class UserEntity {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private long id;
+
+  private String email;
+  private String password;
+  private String fullName;
+
+  public long getId() {
+    return id;
+  }
+  public void setId(long id) {
+    this.id = id;
+  }
+  public String getEmail() {
+    return email;
+  }
+  public void setEmail(String email) {
+    this.email = email;
+  }
+  public String getPassword() {
+    return password;
+  }
+  public void setPassword(String password) {
+    this.password = password;
+  }
+  public String getFullName() {
+    return fullName;
+  }
+  public void setFullName(String fullName) {
+    this.fullName = fullName;
+  }
 }
