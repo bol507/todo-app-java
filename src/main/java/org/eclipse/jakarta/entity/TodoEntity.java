8edec3837148e45efbd991165037c4adc5fc1d44
Bolivar Delgado <bolivar.delgado@gmail.com>
2025-02-13 13:36:48 -0500

    feat: validation Todo entity

--- src/main/java/org/eclipse/jakarta/entity/TodoEntity.java
+++ src/main/java/org/eclipse/jakarta/entity/TodoEntity.java
@@ -2,22 +2,36 @@ package org.eclipse.jakarta.entity;
 
 import java.time.LocalDate;
 
+import jakarta.json.bind.annotation.JsonbDateFormat;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @Entity
 public class TodoEntity extends AbstractEntity {
 
+  @NotEmpty(message = "A todo task must be set")
+  @Size(min=3, max = 150, message="The minimum character length should be 3 and max 150.")
   private String task;
+  
+  @NotNull(message="Due date must be set")
+  @FutureOrPresent(message="Due date must be in the present or future.")
+  @JsonbDateFormat(value="yyyy-MM-dd")
   private LocalDate dueDate;
+
+  
+
   private LocalDate dateCreated;
   private boolean isCompleted;
   private boolean isArchived;
   private boolean isRemaind;
 
   @ManyToOne
-  @JoinColumn(name = "TodoUser_id")
+  @JoinColumn(name = "User_id")
   private UserEntity todoOwner;
 
   public String getTask() {
